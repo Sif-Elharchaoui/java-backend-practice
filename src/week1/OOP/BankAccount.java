763052ea1aa02a3c0f4418368f9f647edcc9850b
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-08-17 18:41:30 +0800

    feat: BankAccount class - encapsulation, constructors, this keyword

---
+++ src/week1/OOP/BankAccount.java
@@ -0,0 +1,44 @@
+package week1.OOP;
+
+public class BankAccount {
+
+    private String ownerName;
+    private double balance;
+
+    public BankAccount() {
+        this("Unknown", 0.0);
+    }
+
+    public BankAccount(String ownerName, double balance) {
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid withdrawal amount.");
+        } else if (amount > balance) {
+            System.out.println("Insufficient funds.");
+        } else {
+            balance -= amount;
+            System.out.println("Withdrew: " + amount);
+        }
+    }
+}
