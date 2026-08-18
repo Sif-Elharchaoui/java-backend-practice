1df16287bddef77b4d79bee5b66116f13136dd2c
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-08-18 23:03:25 +0800

    feat: Shape hierarchy - inheritance with Circle, Rectangle, Triangle

---
+++ src/week1/OOP/Shape.java
@@ -0,0 +1,21 @@
+package week1.OOP;
+
+public class Shape {
+    private String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double area() {
+        return 0;
+    }
+
+    public double perimeter() {
+        return 0;
+    }
+}
