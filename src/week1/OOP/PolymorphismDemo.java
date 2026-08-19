475ef05820500b4b0e21d38fb81ea484f842dc4c
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-08-19 22:51:45 +0800

    feat: polymorphism - Shape[] array with dynamic method dispatch

---
+++ src/week1/OOP/PolymorphismDemo.java
@@ -0,0 +1,20 @@
+package week1.OOP;
+
+public class PolymorphismDemo {
+    public static void main(String[] args) {
+        Shape[] shapes = {
+                new Circle(5),
+                new Rectangle(4, 6),
+                new Triangle(6, 4, 5, 5, 6)
+        };
+
+        double totalArea = 0;
+
+        for (Shape s : shapes) {
+            System.out.printf("%s - Area: %.2f, Perimeter: %.2f%n", s.getName(), s.area(), s.perimeter());
+            totalArea += s.area();
+        }
+
+        System.out.printf("Total area of all shapes: %.2f%n", totalArea);
+    }
+}
