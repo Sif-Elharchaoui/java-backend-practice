60bed59f9ff0da4b328e88973cdf23dfc419cf36
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-08-23 20:53:00 +0800

    feat: Library System - Loan class demonstrating composition

---
+++ src/week1/librarysystem/Loan.java
@@ -0,0 +1,34 @@
+package week1.librarysystem;
+
+public class Loan {
+
+    private Book book;
+    private Member member;
+    private String borrowDate;
+
+    public Loan(Book book, Member member, String borrowDate) {
+        this.book = book;
+        this.member = member;
+        this.borrowDate = borrowDate;
+
+        book.setAvailable(false);
+    }
+
+    public void returnBook() {
+        book.setAvailable(true);
+
+        System.out.println(
+                book.getTitle() + " returned by " + member.getName()
+        );
+    }
+
+    public void printLoanInfo() {
+        System.out.println(
+                member.getName()
+                        + " borrowed \""
+                        + book.getTitle()
+                        + "\" on "
+                        + borrowDate
+        );
+    }
+}
