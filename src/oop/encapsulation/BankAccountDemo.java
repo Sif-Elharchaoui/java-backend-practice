c3cd3883c8a76b4a5ebbe42da3baa8f336837f6d
Sif-Elharchaoui <315746660+Sif-Elharchaoui@users.noreply.github.com>
2026-08-27 20:37:17 +0800

    fix: rename Main class to BankAccountDemo to match filename

--- src/oop/encapsulation/BankAccountDemo.java
+++ src/oop/encapsulation/BankAccountDemo.java
@@ -2,7 +2,7 @@ package oop.encapsulation;
 
 import java.util.Scanner;
 
-public class Main {
+ public class BankAccountDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
